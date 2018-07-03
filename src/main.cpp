69e82fac69195b896853111b8141982e7eae0b50
ExpiredPopsicle <expiredpopsicle@gmail.com>
2018-07-02 22:43:47 -0700

    Done.

--- src/main.cpp
+++ src/main.cpp
@@ -71,12 +71,9 @@ ExPop::PixelImage<uint8_t> *pixelImageLoadPNG(const std::string &data)
 
                         png_read_update_info(png_ptr, info_ptr);
 
-
                         width = png_get_image_width(png_ptr, info_ptr);
                         height = png_get_image_height(png_ptr, info_ptr);
                         colorType = png_get_color_type(png_ptr, info_ptr);
-                        bitDepth = png_get_bit_depth(png_ptr, info_ptr);
-
 
                         rowBytes = png_get_rowbytes(png_ptr, info_ptr);
                         size_t byteCount = rowBytes * height;
@@ -443,6 +440,124 @@ bool pixelImageSaveToFile(
     return ret;
 }
 
+ExPop::PixelImage<uint8_t> *extractLuminance(const ExPop::PixelImage<uint8_t> &img)
+{
+    ExPop::PixelImage<uint8_t> *ret = new ExPop::PixelImage<uint8_t>(
+        img.getWidth(), img.getHeight(), 1);
+
+    if(img.getChannelCount() == 3 || img.getChannelCount() == 4) {
+
+        for(ExPop::PixelImage_Dimension y = 0; y < img.getHeight(); y++) {
+            for(ExPop::PixelImage_Dimension x = 0; x < img.getWidth(); x++) {
+
+                const ExPop::PixelImage<uint8_t>::PixelValueType &rv =
+                    img.getData(x, y, 0);
+                const ExPop::PixelImage<uint8_t>::PixelValueType &gv =
+                    img.getData(x, y, 1);
+                const ExPop::PixelImage<uint8_t>::PixelValueType &bv =
+                    img.getData(x, y, 2);
+                float rf = rv.getScaledValue<float>();
+                float gf = gv.getScaledValue<float>();
+                float bf = bv.getScaledValue<float>();
+
+                float lum =
+                    0.2126f * rf +
+                    0.7152f * gf +
+                    0.0722f * bf;
+
+                ret->getData(x, y, 0).setScaledValue<float>(lum);
+            }
+        }
+
+    } else if(img.getChannelCount() == 1 || img.getChannelCount() == 2) {
+
+        // Just copy the grey channel right over.
+        for(ExPop::PixelImage_Dimension y = 0; y < img.getHeight(); y++) {
+            for(ExPop::PixelImage_Dimension x = 0; x < img.getWidth(); x++) {
+                ret->getData(x, y, 0) = img.getData(x, y, 0);
+            }
+        }
+
+    } else {
+
+        delete ret;
+        ret = nullptr;
+
+    }
+
+    return ret;
+}
+
+ExPop::PixelImage<uint8_t> *sobelFilter(const ExPop::PixelImage<uint8_t> &img)
+{
+    ExPop::PixelImage<uint8_t> *ret = new ExPop::PixelImage<uint8_t>(
+        img.getWidth(), img.getHeight(), 1);
+
+    assert(img.getChannelCount() == 1);
+
+    const float kernelH[3][3] = {
+        {  1,  0, -1 },
+        {  2,  0, -2 },
+        {  1,  0, -1 }
+    };
+
+    const float kernelV[3][3] = {
+        {  1,  2,  1 },
+        {  0,  0,  0 },
+        { -1, -2, -1 }
+    };
+
+    for(ExPop::PixelImage_Dimension y = 0; y < img.getHeight(); y++) {
+        for(ExPop::PixelImage_Dimension x = 0; x < img.getWidth(); x++) {
+
+            float magH = 0.0f;
+            float magV = 0.0f;
+
+            for(int k = 0; k < 3; k++) {
+                for(int j = 0; j < 3; j++) {
+                    float v = img.getData(
+                        x + k - 1,
+                        y + j - 1,
+                        0,
+                        ExPop::PixelImage_EdgeMode_Clamp).getScaledValue<float>();
+
+                    magH += kernelH[k][j] * v;
+                    magV += kernelV[k][j] * v;
+                }
+            }
+
+            float mag = sqrt(magH * magH + magV * magV);
+
+            ret->getData(x, y, 0).setScaledValue(mag);
+        }
+    }
+
+    return ret;
+}
+
+float getRowOrColValue(
+    const ExPop::PixelImage<uint8_t> &img,
+    int bigAxis,
+    ExPop::PixelImage_Dimension axisPos)
+{
+    ExPop::PixelImage_Dimension axes[2] = {
+        img.getWidth(),
+        img.getHeight()
+    };
+
+    float mag = 0.0f;
+
+    for(ExPop::PixelImage_Dimension pos = 0; pos < axes[!bigAxis]; pos++) {
+        ExPop::PixelImage_Dimension fullPos[2];
+        fullPos[!bigAxis] = pos;
+        fullPos[bigAxis]  = axisPos;
+
+        mag += img.getData(fullPos[0], fullPos[1], 0).getScaledValue<float>();
+    }
+
+    return mag;
+}
+
 int main(int argc, char *argv[])
 {
     ExPop::CommandlineParser cmdParser(argv[0]);
@@ -501,15 +616,145 @@ itself for Grayscale and Grayscale + alpha images.)",
         return 1;
     }
 
+    if(height <= 0 || width <= 0) {
+        std::cerr << "Error: Invalid size specified." << std::endl;
+        return 1;
+    }
+
+    float desiredAspectRatio = float(width) / float(height);
+
     for(size_t i = 0; i < inputFilenames.size(); i++) {
+
         ExPop::PixelImage<uint8_t> *img = pixelImageLoadFromFile(inputFilenames[i]);
 
-        std::cout << "Image: " << img << std::endl;
+        std::cout << "Processing: " << inputFilenames[i] << std::endl;
 
         if(img) {
-            pixelImageSaveTGAToFile(*img, "out.tga");
+            pixelImageSaveToFile(*img, "out.tga");
+        } else {
+            std::cerr << "Error: Failed to load image: " << inputFilenames[i] << std::endl;
+            std::cerr << "       Unsupported image format or type." << std::endl;
+            return 1;
         }
 
+
+        ExPop::PixelImage<uint8_t> *lum = extractLuminance(*img);
+        ExPop::PixelImage<uint8_t> *sobel = sobelFilter(*lum);
+
+        pixelImageSaveToFile(*lum, "lum.png");
+        pixelImageSaveToFile(*sobel, "sobel.png");
+
+        float inputAspectRatio =
+            float(img->getWidth()) /
+            float(img->getHeight());
+
+        std::cout << "Input aspect ratio: " << inputAspectRatio << std::endl;
+        std::cout << "Desired aspect ratio: " << desiredAspectRatio << std::endl;
+
+        ExPop::PixelImage_Dimension newHeight = 0;
+        ExPop::PixelImage_Dimension newWidth = 0;
+        int bigAxis = 0;
+
+        if(inputAspectRatio > desiredAspectRatio) {
+            // Width extends beyond desired size.
+            newHeight = height;
+            newWidth = height * (inputAspectRatio);
+            bigAxis = 0;
+            std::cout << "Image size now (1): " << newWidth << ", " << newHeight << std::endl;
+        } else {
+            // Height extends beyond desired size or whatever.
+            newHeight = width * (1.0f / inputAspectRatio);
+            newWidth = width;
+            bigAxis = 1;
+            std::cout << "Image size now (2): " << newWidth << ", " << newHeight << std::endl;
+        }
+
+        ExPop::PixelImage<uint8_t> *scaledSobel = ExPop::pixelImageScale<uint8_t>(*sobel, newWidth, newHeight);
+        ExPop::PixelImage<uint8_t> *scaledImg = ExPop::pixelImageScale<uint8_t>(*img, newWidth, newHeight);
+
+
+        pixelImageSaveToFile(*scaledImg, "scaledimg.png");
+        pixelImageSaveToFile(*scaledSobel, "scaledsobel.png");
+
+
+
+        ExPop::PixelImage_Dimension axes[2] = {
+            scaledSobel->getWidth(),
+            scaledSobel->getHeight()
+        };
+
+        ExPop::PixelImage_Dimension newAxes[2] = {
+            width,
+            height
+        };
+
+
+        float biggestWindowValue = 0.0f;
+        ExPop::PixelImage_Dimension biggestWindowStart = 0;
+        float currentWindowValue = 0.0f;
+        std::vector<float> rowValues;
+
+        for(ExPop::PixelImage_Dimension pos = 0; pos < axes[bigAxis]; pos++) {
+            rowValues.push_back(getRowOrColValue(*scaledSobel, bigAxis, pos));
+        }
+
+        // Build up initial window.
+        for(ExPop::PixelImage_Dimension i = 0; i < newAxes[bigAxis]; i++) {
+            currentWindowValue += rowValues[i];
+        }
+
+        std::cout << "Initial window: " << currentWindowValue << std::endl;
+        biggestWindowStart = 0;
+        biggestWindowValue = currentWindowValue;
+
+        std::cout << "newAxes[bigAxis]: " << newAxes[bigAxis] << std::endl;
+        std::cout << "axes[bigAxis]:    " << axes[bigAxis] << std::endl;
+
+        for(ExPop::PixelImage_Dimension i = newAxes[bigAxis] + 1; i < axes[bigAxis]; i++) {
+            ExPop::PixelImage_Dimension startRow = i - newAxes[bigAxis];
+            currentWindowValue -= rowValues[startRow - 1];
+            currentWindowValue += rowValues[i];
+
+            if(biggestWindowValue < currentWindowValue) {
+                biggestWindowValue = currentWindowValue;
+                biggestWindowStart = startRow;
+            }
+            std::cout << currentWindowValue << std::endl;
+        }
+
+        std::cout << "Startrow: " << biggestWindowStart << std::endl;
+
+        ExPop::PixelImage<uint8_t> outputImage(width, height, scaledImg->getChannelCount());
+        for(ExPop::PixelImage_Dimension n = 0; n < newAxes[!bigAxis]; n++) {
+            for(ExPop::PixelImage_Dimension i = 0; i < newAxes[bigAxis]; i++) {
+
+                ExPop::PixelImage_Dimension dstpos[2];
+                dstpos[bigAxis] = i;
+                dstpos[!bigAxis] = n;
+
+                ExPop::PixelImage_Dimension srcpos[2];
+                srcpos[bigAxis] = i + biggestWindowStart;
+                srcpos[!bigAxis] = n;
+
+                for(ExPop::PixelImage_Dimension c = 0; c < scaledImg->getChannelCount(); c++) {
+                    ExPop::PixelImage<uint8_t>::PixelValueType &vOut = outputImage.getData(dstpos[0], dstpos[1], c);
+                    ExPop::PixelImage<uint8_t>::PixelValueType &vIn = scaledImg->getData(srcpos[0], srcpos[1], c);
+
+                    vOut = vIn;
+                }
+            }
+        }
+
+        if(outputFilename.size() != 0) {
+            pixelImageSaveToFile(outputImage, outputFilename);
+        } else {
+            pixelImageSaveToFile(outputImage, inputFilenames[i]);
+        }
+
+        delete sobel;
+        delete scaledImg;
+        delete scaledSobel;
+        delete lum;
         delete img;
     }
 
